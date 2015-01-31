4f911e99a70832765b7af2a721e71fc2889866f4
yaodaniel <Daniel988@ucla.edu>
2015-01-31 00:29:46 -0800

    source codes

---
+++ android/src/com/saver/shower/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.saver.shower.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.saver.shower.splashScreen;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new splashScreen(), config);
+	}
+}
---
+++ desktop/src/com/saver/shower/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.saver.shower.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.saver.shower.splashScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new splashScreen(), config);
+	}
+}
