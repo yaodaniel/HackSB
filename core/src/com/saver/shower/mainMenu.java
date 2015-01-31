de16db5cfb22511b540ef6b5c745a30f9c74cd41
Katya <kpfomenko@gmail.com>
2015-01-31 06:23:30 -0800

    Updating MainMenu.java

--- core/src/com/saver/shower/mainMenu.java
+++ core/src/com/saver/shower/mainMenu.java
@@ -1,11 +1,46 @@
 package com.saver.shower;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 
 public class mainMenu implements Screen{
+	public BitmapFont white;
+	private splashScreen object;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture img;
+	private TextButton button_start, button_history;
+	private TextureAtlas atlas;
+	private Skin skin;
+	private Stage stage;
+	private boolean checked;
 	
 	public mainMenu(splashScreen obj){
-		
+		this.object = obj;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 600);
+		img = new Texture("SS2.bmp");
+		batch = new SpriteBatch();
+		//Button Stuff
+		atlas = new TextureAtlas(Gdx.files.internal("button.pack"));
+		skin = new Skin(atlas);
+		stage = new Stage(new ScreenViewport());
+		white = obj.white;
 	}
 
 	@Override
@@ -16,14 +51,65 @@ public class mainMenu implements Screen{
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		
+		stage.act(delta);
+		batch.begin();
+		batch.draw(img,Gdx.graphics.getWidth()/2-img.getWidth()/2,Gdx.graphics.getHeight()/2-img.getHeight()/2);
+		stage.draw();
+		batch.end();
 		
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+		camera.setToOrtho(false, 800, 600);
 		
+		if(stage == null){
+			stage.getViewport().update(width, height, true);
+		}
+		if(stage.getWidth()!=width || stage.getHeight()!= height){
+			stage.dispose();
+			stage.getViewport().update(width, height, true);
+		}
+		stage.clear();
+		Gdx.input.setInputProcessor(stage);
+		TextButtonStyle style = new TextButtonStyle();
+		style.up = skin.getDrawable("button.up");
+		style.down = skin.getDrawable("button.down");
+		style.over = skin.getDrawable("button.down");
+		//style.font = white;
+		button_start = new TextButton("Start", style);
+		button_start.setWidth(Gdx.graphics.getWidth()/1.5f);
+		button_start.setHeight(Gdx.graphics.getHeight()/8);
+		button_start.setX(Gdx.graphics.getWidth()/2- button_start.getWidth()/2);
+		button_start.setY(Gdx.graphics.getHeight()/3- button_start.getHeight()/2);
+		button_start.addListener(new InputListener() {
+			public boolean touchDown (InputEvent event, float x, float y,
+										int pointer, int button) {
+				checked = true;
+				Gdx.app.log(showerSaver.LOG, "Button Checked!");
+				return true;
+			}
+			public void touchUp(InputEvent event, float x, float y,
+									int pointer, int button) {
+				if(checked)
+				Gdx.app.log(showerSaver.LOG, "Button Not Checked!");
+				checked = false;
+			}
+			public void touchDragged(InputEvent event, float x, float y, int pointer)
+			{
+				if(!button_start.isPressed()){
+					checked = false;
+					Gdx.app.log(showerSaver.LOG, "Button [almost] Checked!");
+				}else{
+					checked = true;
+				}
+			}
+		});
+		stage.addActor(button_start);
 	}
 
 	@Override
@@ -47,7 +133,12 @@ public class mainMenu implements Screen{
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		atlas.dispose();
+		batch.dispose();
+		skin.dispose();
+		stage.clear();
+		stage.dispose();
+		button_start.remove();
 	}
 
 }
