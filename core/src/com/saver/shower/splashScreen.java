183efe378c6caf12a1c8fac8aee4250312334c36
yaodaniel <Daniel988@ucla.edu>
2015-01-31 04:18:29 -0800

    added splashScreen tweening

--- core/src/com/saver/shower/splashScreen.java
+++ core/src/com/saver/shower/splashScreen.java
@@ -1,18 +1,27 @@
 package com.saver.shower;
 
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class splashScreen implements Screen{
 	private showerSaver object;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture img;
-	//private TweenManager manager;
+	private Sprite splashSprite;
+	private Texture img, splashTexture;
+	private TweenManager manager;
 	
 	public splashScreen(showerSaver obj) {
 		this.object = obj;
@@ -23,18 +32,41 @@ public class splashScreen implements Screen{
 
 	@Override
 	public void show() {
+		splashTexture = new Texture("splashscreen.jpeg");
+		splashTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
+		splashSprite = new Sprite(splashTexture);
+		splashSprite.setColor(1,1,1,0);
 		
+		Tween.registerAccessor(Sprite.class, new SpriteTween());
+		manager = new TweenManager();
+		TweenCallback cb = new TweenCallback(){
+
+			@Override
+			public void onEvent(int type, BaseTween<?> source) {
+				tweencompleted();
+			}
+		};
+		
+		Tween.to(splashSprite, SpriteTween.ALPHA, 2f).target(1).ease(TweenEquations.easeInQuad).repeatYoyo(1,2.5f).setCallback(cb).setCallbackTriggers(TweenCallback.COMPLETE).start(manager);
+		
+	}
+	
+	private void tweencompleted(){
+		//status = true;
+		Gdx.app.log(showerSaver.LOG, "Tween Complete");
 	}
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		manager.update(delta);
 		camera.update();
 		//batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img,Gdx.graphics.getWidth()/2-img.getWidth()/2,Gdx.graphics.getHeight()/2-img.getHeight()/2);
+		splashSprite.draw(batch);
 		batch.end();
 		
 	}
