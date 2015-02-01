761c66cf35922f3618d6be04b43f959a6bbca146
yaodaniel <Daniel988@ucla.edu>
2015-01-31 18:27:05 -0800

    added new buttons

--- core/src/com/saver/shower/showerSaver.java
+++ core/src/com/saver/shower/showerSaver.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.saver.shower.splashScreen;
 
 public class showerSaver extends Game {
 	
-	public static float timer = 420f;
+	public static float timer = 360f;
+	public static int minute = (int) (showerSaver.timer/60);
+	public static int seconds = (int) (showerSaver.timer%60);
 	public static final String VERSION = "4.19";
 	public static final String LOG = "Hackathon_app";
 	public static AssetManager loadManager = new AssetManager();
@@ -20,6 +23,10 @@ public class showerSaver extends Game {
 		loadManager.load("gamefont_0.png",Texture.class);
 		loadManager.load("gamefont_1.png",Texture.class);
 		loadManager.load("gamefont.fnt",BitmapFont.class);
+		loadManager.load("button.pack", TextureAtlas.class);
+		loadManager.load("button.png", Texture.class);
+		loadManager.load("buttons.pack", TextureAtlas.class);
+		loadManager.load("buttons.png", Texture.class);
 		setScreen(new splashScreen(this));
 	}	
 }
--- core/src/com/saver/shower/timerSetup.java
+++ core/src/com/saver/shower/timerSetup.java
@@ -5,21 +5,28 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class timerSetup implements Screen{
 	
+	private showerSaver appObject;
 	private LabelStyle displayTimeStyle;
 	private Label displayTime;
-	private showerSaver appObject;
 	private Stage stage;
 	private SpriteBatch batch;
-	int minute = (int) (showerSaver.timer/60);
-	int seconds = (int) (showerSaver.timer%60);
+	private TextButton button_back, button_next, button_inc_minute,
+					   button_inc_second, button_dec_minute, button_dec_second;
+	private TextButtonStyle style = new TextButtonStyle();
+	private Skin skin;
+	private TextureAtlas atlas;
 	
 	public timerSetup(showerSaver appObject) {
 		// TODO Auto-generated constructor stub
@@ -29,7 +36,21 @@ public class timerSetup implements Screen{
 		displayTimeStyle = new LabelStyle(showerSaver.white,Color.WHITE);
 		stage = new Stage(new ScreenViewport());
 		batch = new SpriteBatch();
-		displayTime = new Label(String.format("%d:%02d",minute, seconds), displayTimeStyle);
+		displayTime = new Label(String.format("%d:%02d",showerSaver.minute, showerSaver.seconds), displayTimeStyle);
+		atlas = showerSaver.loadManager.get("buttons.pack");
+		skin = new Skin(atlas);
+		style = new TextButtonStyle();
+		style.up = skin.getDrawable("SSbutt");
+		style.down = skin.getDrawable("SSbuttclick");
+		style.over = skin.getDrawable("SSbuttclick");
+		style.font = showerSaver.white;
+		button_back = new TextButton("<--", style);
+		button_back.setWidth(Gdx.graphics.getWidth()/7f);
+		button_back.setHeight(Gdx.graphics.getHeight()/10f);
+		button_back.setX(Gdx.graphics.getWidth()/4 - button_back.getWidth()/2);
+		button_back.setY(button_back.getHeight());
+		button_next = new TextButton("-->", style);
+		stage = new Stage(new ScreenViewport());
 	}
 	@Override
 	public void show() {
@@ -42,11 +63,13 @@ public class timerSetup implements Screen{
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if(seconds >= 59)
-			seconds = 0;
+		if(showerSaver.seconds >= 59){
+			showerSaver.minute++;
+			showerSaver.seconds = 0;
+		}
 		else
-			seconds++;
-		displayTime.setText(String.format("%d:%02d",minute,seconds));
+			showerSaver.seconds++;
+		displayTime.setText(String.format("%d:%02d",showerSaver.minute,showerSaver.seconds));
 		stage.act(delta);
 		batch.begin();
 		stage.draw();
@@ -75,6 +98,7 @@ public class timerSetup implements Screen{
 		displayTime.setAlignment(Align.center);
 		displayTime.setFontScale(1.3f);
 		stage.addActor(displayTime);
+		stage.addActor(button_back);
 	}
 
 	@Override
